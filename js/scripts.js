b1d0df385739dd3177364779b1756bb5ce9a6119
DavideBernardini <davidebernardini09@gmail.com>
2021-07-29 15:22:12 +0200

    calcolato prezzo intero

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+// Chiedere all'utente il numero di chilometri che vuole percorrere
+var km = parseInt(prompt("Indicare il numero di kilometri da percorrere"));
+// Chiedere all'utente l'età del passeggero
+var age = parseInt(prompt("Indicare l'età del passeggero")); 
+// calcora prezzo biglietto intero
+var fullprice = km * 0.21;
+
+
