5ed6888886ec0b5555e58377d7a18e5b3ddf3641
DavideBernardini <davidebernardini09@gmail.com>
2021-07-30 11:01:11 +0200

    refactoring

--- js/scripts.js
+++ js/scripts.js
@@ -6,9 +6,9 @@ var age = parseInt(prompt("Indicare l'età del passeggero"));
 var price = parseInt(km * 0.21);
 // calcolare prezzo ridotto per minorenni e over 65
 if (age <= 18) {
-    price = price - (price * 20) / 100
+    price = price - (price * 20) / 100;
 } else if (age >= 65) {
-    price = price - (price * 40) / 100
+    price = price - (price * 40) / 100;
 }
 // Aggiungere totale biglietto nell'html
 document.getElementById("ticket_price").innerHTML += price.toFixed(2) + "€";
